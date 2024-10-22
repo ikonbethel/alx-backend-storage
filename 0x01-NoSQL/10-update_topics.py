4bd876f86c590fb079e62acd42d7ead6bbdf4cfa
Bethel Ikonne <46830724+ikonbethel@users.noreply.github.com>
2024-10-22 13:53:49 -0700

    Create 10-update_topics.py

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+'''Task 10: Change school topics'''
+
+
+def update_topics(mongo_collection, name, topics):
+    '''
+    Changes all topics of a school document based on the name:
+    mongo_collection will be the pymongo collection object
+    name (string) will be the school name to update
+    topics (list of strings) will be the list of topics approached in the school.
+    '''
+    mongo_collection.update_many(
+            {'name': name},
+            {'$set': {'topics': topics}}
+    )
