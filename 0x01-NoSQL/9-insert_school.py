b0ecb284bf8a4118754cb0138e838fc5251bd0d6
Bethel Ikonne <46830724+ikonbethel@users.noreply.github.com>
2024-10-22 19:41:40 -0700

    Update 9-insert_school.py

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
-'''Task 9: Insert a document in python'''
+"""
+Insert a document in Python
+"""
 
 
 def insert_school(mongo_collection, **kwargs):
-    '''
-    Inserts a new document in a collection based on kwargs.
-    mongo_collection will be the pymongo collection object
-    Returns the new _id.
-    '''
-    new_id = mongo_collection.insert_one(kwargs)
-    return new_id
+    """
+     inserts a new document in a
+      collection based on kwargs
+
+    :param mongo_collection:
+    :param kwargs:
+    :return:
+    """
+    new_documents = mongo_collection.insert_one(kwargs)
+    return new_documents.inserted_id
